41a3e93a61ce871d8036e3fd5c808ef6baca8438
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2025-05-02 23:29:05 +0200

    Backend 	Configuración del Proyecto

---
+++ backendPruebaTecnica/src/test/java/es/econocom/backendPruebaTecnica/BackendPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.econocom.backendPruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
