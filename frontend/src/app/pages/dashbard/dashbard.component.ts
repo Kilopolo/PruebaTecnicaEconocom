93f0c9194a5350ed9739504a6881fbf9fb0b68c7
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2025-05-03 14:48:48 +0200

    creados los componentes de header, footer, dashboard y login

---
+++ frontend/src/app/pages/dashbard/dashbard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbard',
+  imports: [],
+  templateUrl: './dashbard.component.html',
+  styleUrl: './dashbard.component.scss'
+})
+export class DashbardComponent {
+
+}
