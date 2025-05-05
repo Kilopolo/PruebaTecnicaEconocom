a414ffcba99687c65421186349abaf186934198e
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2025-05-05 13:01:46 +0200

    reordenacion ffrontend carpetas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { DashbardComponent } from "./components/pages/dashbard/dashbard.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { LoginComponent } from "./components/auth/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  DashbardComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
